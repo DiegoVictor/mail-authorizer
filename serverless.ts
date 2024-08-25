e5d4209f56884c5bd3f8c6ed710eb96da9dc4db5
Diego Victor <diegovictorgonzaga@gmail.com>
2024-08-25 00:25:31 -0300

    Set JWT secret environment variable

--- serverless.ts
+++ serverless.ts
@@ -16,6 +16,7 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      JWT_SECRET: secret(),
     },
   },
   functions: {
