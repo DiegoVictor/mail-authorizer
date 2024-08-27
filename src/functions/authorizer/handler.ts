3a5757b8191b7f00a7f627b66b43aca640d6219e
Diego Victor <diegovictorgonzaga@gmail.com>
2024-08-27 09:19:18 -0300

    Verify token provided

--- src/functions/authorizer/handler.ts
+++ src/functions/authorizer/handler.ts
@@ -1,5 +1,14 @@
 import { APIGatewayProxyEventHeaders } from 'aws-lambda';
+import { verify } from 'jsonwebtoken';
+
 export const main = async (event: APIGatewayProxyEventHeaders) => {
   console.log(JSON.stringify(event));
 
+  try {
+    const { authorizationToken, methodArn } = event;
+    const token = authorizationToken.split(' ').pop();
+
+    verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+  }
 };
