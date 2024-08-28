1ef2d3bc365c467a29f7f537a0fabaafb5bed2b0
Diego Victor <diegovictorgonzaga@gmail.com>
2024-08-28 09:33:51 -0300

    Authorize request

--- src/functions/authorizer/handler.ts
+++ src/functions/authorizer/handler.ts
@@ -1,4 +1,5 @@
 import { APIGatewayProxyEventHeaders } from 'aws-lambda';
+import { randomUUID } from 'crypto';
 import { verify } from 'jsonwebtoken';
 
 export const main = async (event: APIGatewayProxyEventHeaders) => {
@@ -9,6 +10,19 @@ export const main = async (event: APIGatewayProxyEventHeaders) => {
     const token = authorizationToken.split(' ').pop();
 
     verify(token, process.env.JWT_SECRET);
+    return {
+      principalId: randomUUID(),
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: methodArn,
+          },
+        ],
+      },
+    };
   } catch (err) {
   }
 };
