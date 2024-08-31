6c85b110bb27888c42a52598634bb05f9c12f1ce
Diego Victor <diegovictorgonzaga@gmail.com>
2024-08-31 13:27:20 -0300

    Use timestamp for file id

--- scripts/upload-file.sh
+++ scripts/upload-file.sh
@@ -2,16 +2,10 @@ filename=8af6860c-d544-4a6f-abcd-49871dde7bbd.mp4
 
 aws s3 cp sample.mp4 s3://mailauthorizer-content/$filename
 aws dynamodb put-item \
-  --table-name file \
+  --table-name files \
   --item '{
-    "id": {
-      "S": "34140420-f761-418f-9a29-817204f7ea9d"
-    },
-    "name": {
-      "S": "$filename"
-    },
-    createdAt: {
-      "S": "2024-08-16T10:00:00Z"
-    }
+    "id": { "S": "1725059895044" },
+    "title": { "S": "Asian Skater Boys" },
+    "key": { "S": "$filename" }
   }' \
   --return-consumed-capacity TOTAL
