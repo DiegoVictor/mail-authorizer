8d56f9c18e1f75f653344099e295c51ce240e72c
Diego Victor <diegovictorgonzaga@gmail.com>
2024-09-01 10:24:30 -0300

    Validate id

--- src/functions/files/get-signed-url/handler.ts
+++ src/functions/files/get-signed-url/handler.ts
@@ -1,5 +1,21 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
+import { z } from 'zod';
 
 export const main = async (event: APIGatewayProxyEvent) => {
   console.log(JSON.stringify(event));
+
+  const schema = z.object({
+    id: z.string().uuid(),
+  });
+  const { success, error, data } = schema.safeParse(event.pathParameters);
+
+  if (!success) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Validation Failed',
+        issues: error.issues,
+      }),
+    };
+  }
 };
