41daf23bb6f5e98f0b8d5d450b6c89117505764b
Diego Victor <diegovictorgonzaga@gmail.com>
2024-09-02 07:59:48 -0300

    Retrieve file from dynamodb

--- src/use-cases/generate-signed-url.ts
+++ src/use-cases/generate-signed-url.ts
@@ -1,7 +1,28 @@
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+import { FILES_TABLE_NAME } from '@libs/constants';
 
 const generateSignedUrl = async (id: string) => {
   const dynamodb = new DynamoDB();
+  const file = await dynamodb
+    .query({
+      TableName: FILES_TABLE_NAME,
+      KeyConditions: {
+        id: {
+          ComparisonOperator: 'EQ',
+          AttributeValueList: [marshall(id)],
+        },
+      },
+      ScanIndexForward: false,
+      Limit: 1,
+    })
+    .then(({ Items }) => {
+      if (Array.isArray(Items) && Items.length > 0) {
+        return unmarshall(Items.shift());
+      }
+
+      return null;
+    });
 };
 
 export { generateSignedUrl };
