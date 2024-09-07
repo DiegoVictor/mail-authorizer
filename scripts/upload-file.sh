d2fd7d9112f70d06f06f95101ccb6fbb69651985
Diego Victor <diegovictorgonzaga@gmail.com>
2024-09-07 02:27:03 -0300

    Update upload file script

--- scripts/upload-file.sh
+++ scripts/upload-file.sh
@@ -1,11 +1,12 @@
 filename=8af6860c-d544-4a6f-abcd-49871dde7bbd.mp4
 
-aws s3 cp sample.mp4 s3://mailauthorizer-content/$filename
-aws dynamodb put-item \
-  --table-name files \
-  --item '{
-    "id": { "S": "1725059895044" },
-    "title": { "S": "Asian Skater Boys" },
-    "key": { "S": "$filename" }
-  }' \
-  --return-consumed-capacity TOTAL
+aws s3 cp sample.mp4 s3://mailauthorizer-content/files/$filename
+
+aws s3api put-object \
+  --bucket mailauthorizer-content \
+  --key files/$filename \
+  --metadata '{"filename":"sample.mp4","title":"Asian Skater Boys"}'
+
+sls invoke --function postProcessing \
+  --data '{"Records":[{"s3":{"object":{"key":"files/8af6860c-d544-4a6f-abcd-49871dde7bbd.mp4"}}}]}'
+
