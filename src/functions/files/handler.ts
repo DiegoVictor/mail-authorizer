fca781c45a7054d2f20256417349324180ac5e25
Diego Victor <diegovictorgonzaga@gmail.com>
2024-09-10 09:37:26 -0300

    Validate request payload

--- src/functions/files/handler.ts
+++ src/functions/files/handler.ts
@@ -1,10 +1,17 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { getFiles } from '@use-cases/get-files';
+import { z } from 'zod';
 
 export const main = async (event: APIGatewayProxyEvent) => {
   console.log(JSON.stringify(event));
 
   switch (event.httpMethod) {
+    case 'POST': {
+      const schema = z.object({
+        title: z.string(),
+        filename: z.string(),
+      });
+      const { success, error, data } = schema.safeParse(JSON.parse(event.body));
     default:
     case 'GET': {
       const { data, cursorId } = await getFiles(
