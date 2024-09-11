9eb3687712e8cd6a820c7d0e789169a78de47cef
Diego Victor <diegovictorgonzaga@gmail.com>
2024-09-11 09:04:55 -0300

    Generate upload URL

--- src/use-cases/generate-presigned-url.ts
+++ src/use-cases/generate-presigned-url.ts
@@ -1,9 +1,26 @@
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { S3, PutObjectCommand } from '@aws-sdk/client-s3';
 import { randomUUID } from 'node:crypto';
 import { extname } from 'node:path';
 import { FILES_UPLOAD_FOLDER_NAME } from '@libs/constants';
 
 const generatePresignedUrl = async (title: string, filename: string) => {
   const key = `${FILES_UPLOAD_FOLDER_NAME}/${randomUUID()}${extname(filename)}`;
+
+  const s3 = new S3();
+  const url = await getSignedUrl(
+    s3,
+    new PutObjectCommand({
+      Bucket: process.env.CONTENT_BUCKET,
+      Key: key,
+      Metadata: {
+        title,
+        filename,
+      },
+    })
+  );
+
+  return url;
 };
 
 export { generatePresignedUrl };
