cca05c92a89d5dfe328de273c668972ee9f97533
Diego Victor <diegovictorgonzaga@gmail.com>
2024-09-13 08:41:33 -0300

    Verify if file exists

--- src/use-cases/post-processing-file-upload.ts
+++ src/use-cases/post-processing-file-upload.ts
@@ -1,7 +1,16 @@
 import { S3 } from '@aws-sdk/client-s3';
+import { failure } from '@libs/failure';
 const postProcessingFileUpload = async (key: string) => {
   const s3 = new S3();
 
+  const file = await s3.headObject({
+    Bucket: process.env.CONTENT_BUCKET,
+    Key: key,
+  });
+
+  if (!file) {
+    return failure(404, 'File Not Found');
+  }
 };
 
 export { postProcessingFileUpload };
