78ba10bf22a09fe574944e316c0fda8e85e98347
Diego Victor <diegovictorgonzaga@gmail.com>
2024-09-13 08:42:40 -0300

    Save file dynamodb record

--- src/use-cases/post-processing-file-upload.ts
+++ src/use-cases/post-processing-file-upload.ts
@@ -1,5 +1,10 @@
 import { S3 } from '@aws-sdk/client-s3';
 import { failure } from '@libs/failure';
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { marshall } from '@aws-sdk/util-dynamodb';
+import { randomUUID } from 'crypto';
+import { FILES_TABLE_NAME } from '@libs/constants';
+
 const postProcessingFileUpload = async (key: string) => {
   const s3 = new S3();
 
@@ -16,6 +21,17 @@ const postProcessingFileUpload = async (key: string) => {
     Metadata: { title },
   } = file;
 
+  const dynamodb = new DynamoDB();
+  await dynamodb.putItem({
+    TableName: FILES_TABLE_NAME,
+    Item: marshall({
+      id: randomUUID(),
+      type: 'VIDEO',
+      title,
+      key,
+      createdAt: Date.now(),
+    }),
+  });
 };
 
 export { postProcessingFileUpload };
