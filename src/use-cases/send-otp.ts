3bad96d8b1b11312f250a4ef94d6e021599f99b5
Diego Victor <diegovictorgonzaga@gmail.com>
2024-09-22 10:10:26 -0300

    Use ses service

--- src/use-cases/send-otp.ts
+++ src/use-cases/send-otp.ts
@@ -5,6 +5,7 @@ import { TOTP } from 'totp-generator';
 import { failure } from '@libs/failure';
 import { success } from '@libs/success';
 import { TOTP_TABLE_NAME } from '@libs/constants';
+import { sendEmail } from '@infra/services/ses';
 
 const sendOtp = async (email: string) => {
   const timestamp = Date.now() + 30 * 1000;
@@ -13,35 +14,17 @@ const sendOtp = async (email: string) => {
     timestamp,
   });
 
-  const ses = new SES();
-  const {
-    $metadata: { httpStatusCode },
-  } = await ses.sendEmail({
-    Destination: {
-      ToAddresses: [email],
-    },
-    Message: {
-      Body: {
-        Html: {
-          Charset: 'UTF-8',
-          Data:
-            '<div style="font-family: Roboto, sans-serif;padding: 15px 15px">' +
-            '<div><p>Hi,</p><br /><p>Here is your validation code:</p>' +
-            `<span style="display:inline-block;font-size: 32px;font-weight: bold;margin: 10px 0px">${otp}</span>` +
-            '<p>It is valid for a short period of time, make sure to validate it quickly.</p></div></div>',
-        },
-      },
-      Subject: {
-        Charset: 'UTF-8',
-        Data: 'Verification Code',
-      },
-    },
-    Source: process.env.NOREPLY_EMAIL_ADDRESS,
-  });
-
-  if (httpStatusCode !== 200) {
-    return failure(500, 'Failed to send email. Please try again later.');
-  }
+  await sendEmail(
+    email,
+    `<div style="font-family: Roboto, sans-serif;padding: 15px 15px">
+      <div>
+        <p>Hi,</p><br />
+        <p>Here is your validation code:</p>
+        <span style="display:inline-block;font-size: 32px;font-weight: bold;margin: 10px 0px">${otp}</span>
+        <p>It is valid for a short period of time, make sure to validate it quickly.</p>
+      </div>
+    </div>`
+  );
 
   const expiresAt = new Date(expires);
 
