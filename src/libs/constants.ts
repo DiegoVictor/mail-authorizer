9c31a2edbfcbf747fcf449239065e6c79b287197
Diego Victor <diegovictorgonzaga@gmail.com>
2024-09-24 08:23:22 -0300

    Delete files not used anymore

--- src/libs/constants.ts
+++
@@ -1,4 +0,0 @@
-export const FILES_UPLOAD_FOLDER_NAME = 'files';
-
-export const FILES_TABLE_NAME = 'files';
-export const TOTP_TABLE_NAME = 'totp';
--- src/libs/generate.ts
+++
@@ -1,9 +0,0 @@
-import { randomBytes, randomInt } from 'node:crypto';
-
-export const secret = (size = 16) => randomBytes(size).toString('hex');
-
-export const totp = (size = 8) =>
-  randomBytes(size)
-    .toString('hex')
-    .toUpperCase()
-    .replace(/[0-18-9]/g, () => randomInt(2, 7).toString());
