a599d14c1fd355d37a00f4215066fd194d954a9f
Diego Victor <diegovictorgonzaga@gmail.com>
2024-09-25 18:08:28 -0300

    Use s3 wrapper to get URL

--- src/use-cases/generate-presigned-url.ts
+++ src/use-cases/generate-presigned-url.ts
@@ -1,26 +1,14 @@
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { S3, PutObjectCommand } from '@aws-sdk/client-s3';
 import { randomUUID } from 'node:crypto';
 import { extname } from 'node:path';
-import { FILES_UPLOAD_FOLDER_NAME } from '@libs/constants';
+import { getSignedUrl } from '@infra/services/s3';
 
 const generatePresignedUrl = async (title: string, filename: string) => {
-  const key = `${FILES_UPLOAD_FOLDER_NAME}/${randomUUID()}${extname(filename)}`;
+  const key = `${randomUUID()}${extname(filename)}`;
 
-  const s3 = new S3({
-    endpoint: process.env.IS_OFFLINE ? 'http://localhost:4566' : undefined,
+  const url = await getSignedUrl(key, {
+    title,
+    filename,
   });
-  const url = await getSignedUrl(
-    s3,
-    new PutObjectCommand({
-      Bucket: process.env.CONTENT_BUCKET,
-      Key: key,
-      Metadata: {
-        title,
-        filename,
-      },
-    })
-  );
 
   if (process.env.IS_OFFLINE) {
     return url
