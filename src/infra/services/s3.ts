457213212dc70d2558bc1daa64bed37eee03d7a9
Diego Victor <diegovictorgonzaga@gmail.com>
2024-09-25 18:08:00 -0300

    Create method to generate signed URL

--- src/infra/services/s3.ts
+++ src/infra/services/s3.ts
@@ -1,4 +1,5 @@
-import { S3 } from '@aws-sdk/client-s3';
+import { S3, PutObjectCommand } from '@aws-sdk/client-s3';
+import * as request from '@aws-sdk/s3-request-presigner';
 
 export const FILES_UPLOAD_FOLDER_NAME = 'files';
 
@@ -6,3 +7,16 @@ const s3 = new S3({
   endpoint: process.env.IS_OFFLINE ? 'http://localhost:4566' : undefined,
   forcePathStyle: process.env.IS_OFFLINE ? true : undefined,
 });
+
+export const getSignedUrl = (
+  filename: string,
+  Metadata?: Record<string, string>
+) =>
+  request.getSignedUrl(
+    s3,
+    new PutObjectCommand({
+      Bucket: process.env.CONTENT_BUCKET,
+      Key: `${FILES_UPLOAD_FOLDER_NAME}/${filename}`,
+      Metadata,
+    })
+  );
