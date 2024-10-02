38c2b084b19bc506811c788ddb98cb37fc1a3a1f
Diego Victor <diegovictorgonzaga@gmail.com>
2024-10-02 09:40:06 -0300

    Increase OTP code expiration

--- src/use-cases/send-otp.ts
+++ src/use-cases/send-otp.ts
@@ -6,7 +6,7 @@ import { sendEmail } from '@infra/services/ses';
 const sendOtp = async (email: string) => {
   const timestamp = Date.now() + 30 * 1000;
   const { expires, otp } = TOTP.generate(process.env.TOTP_KEY, {
-    period: 5 * 60,
+    period: 10 * 60,
     timestamp,
   });
 
