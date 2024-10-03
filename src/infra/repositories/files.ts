2f28724e845800088b66a33143be1534c69c16b4
Diego Victor <diegovictorgonzaga@gmail.com>
2024-10-03 17:29:12 -0300

    Move logic to repository

--- src/infra/repositories/files.ts
+++ src/infra/repositories/files.ts
@@ -1,6 +1,52 @@
-import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { DynamoDB, QueryCommandInput } from '@aws-sdk/client-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+import { IFile, IFileType } from '@contracts/file';
+
 const dynamodb = new DynamoDB({
   endpoint: process.env.IS_OFFLINE ? 'http://localhost:4566' : undefined,
 });
 
 export const FILES_TABLE_NAME = 'files';
+
+export const getManyPaginated = async (Limit = 10, cursorId?: string) => {
+  const args: QueryCommandInput = {
+    TableName: FILES_TABLE_NAME,
+    ScanIndexForward: false,
+    Limit,
+    IndexName: 'TypeIndex',
+    KeyConditionExpression: '#type = :type',
+    ExpressionAttributeNames: {
+      '#type': 'type',
+    },
+    ExpressionAttributeValues: {
+      ':type': { S: IFileType.VIDEO },
+    },
+  };
+
+  if (cursorId) {
+    const [id, createdAt] = Buffer.from(cursorId, 'base64')
+      .toString()
+      .split(':');
+
+    args.ExclusiveStartKey = marshall({
+      id,
+      type: IFileType.VIDEO,
+      createdAt: Number(createdAt),
+    });
+  }
+
+  const { Items, LastEvaluatedKey } = await dynamodb.query(args);
+  const data = Items.map((item) => unmarshall(item));
+
+  if (LastEvaluatedKey) {
+    const { id, createdAt } = unmarshall(LastEvaluatedKey);
+    return {
+      data,
+      cursorId: Buffer.from(`${id}:${createdAt}`).toString('base64'),
+    };
+  }
+
+  return {
+    data,
+  };
+};
--- src/use-cases/get-files.ts
+++ src/use-cases/get-files.ts
@@ -1,51 +1,5 @@
-import { DynamoDB, QueryCommandInput } from '@aws-sdk/client-dynamodb';
-import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
-import { FILES_TABLE_NAME } from '@libs/constants';
+import { getManyPaginated } from '@infra/repositories/files';
 
-const getFiles = async (cursorId?: string) => {
-  const args: QueryCommandInput = {
-    TableName: FILES_TABLE_NAME,
-    ScanIndexForward: false,
-    Limit: 10,
-    IndexName: 'TypeIndex',
-    KeyConditionExpression: '#type = :type',
-    ExpressionAttributeNames: {
-      '#type': 'type',
-    },
-    ExpressionAttributeValues: {
-      ':type': { S: 'VIDEO' },
-    },
-  };
-
-  if (cursorId) {
-    const [id, createdAt] = Buffer.from(cursorId, 'base64')
-      .toString()
-      .split(':');
-
-    args.ExclusiveStartKey = marshall({
-      id,
-      type: 'VIDEO',
-      createdAt: Number(createdAt),
-    });
-  }
-
-  const dynamodb = new DynamoDB({
-    endpoint: process.env.IS_OFFLINE ? 'http://localhost:4566' : undefined,
-  });
-
-  const { Items, LastEvaluatedKey } = await dynamodb.query(args);
-  if (LastEvaluatedKey) {
-    const { id, createdAt } = unmarshall(LastEvaluatedKey);
-
-    return {
-      data: Items.map((item) => unmarshall(item)),
-      cursorId: Buffer.from(`${id}:${createdAt}`).toString('base64'),
-    };
-  }
-
-  return {
-    data: Items.map((item) => unmarshall(item)),
-  };
-};
+const getFiles = async (cursorId?: string) => getManyPaginated(10, cursorId);
 
 export { getFiles };
