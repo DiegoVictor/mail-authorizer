a580d8e17b301b28920089a74407c8cdcf867109
Diego Victor <diegovictorgonzaga@gmail.com>
2024-10-05 01:26:30 -0300

    Get object metadata

--- src/use-cases/post-processing-file-upload.ts
+++ src/use-cases/post-processing-file-upload.ts
@@ -1,4 +1,3 @@
-import { S3 } from '@aws-sdk/client-s3';
 import { failure } from '@libs/failure';
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
 import { marshall } from '@aws-sdk/util-dynamodb';
@@ -6,15 +5,7 @@ import { randomUUID } from 'node:crypto';
 import { FILES_TABLE_NAME } from '@libs/constants';
 
 const postProcessingFileUpload = async (key: string) => {
-  const s3 = new S3({
-    endpoint: process.env.IS_OFFLINE ? 'http://localhost:4566' : undefined,
-    forcePathStyle: process.env.IS_OFFLINE ? true : undefined,
-  });
-
-  const file = await s3.headObject({
-    Bucket: process.env.CONTENT_BUCKET,
-    Key: key,
-  });
+  const metadata = await getMetadataByKey(key);
 
   if (!file) {
     return failure(404, 'File Not Found');
