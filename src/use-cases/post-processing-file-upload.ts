5a2eb559ca280c6a30b591dd9f7e7bd10cd695db
Diego Victor <diegovictorgonzaga@gmail.com>
2024-10-05 12:28:30 -0300

    Save into dynamodb

--- src/use-cases/post-processing-file-upload.ts
+++ src/use-cases/post-processing-file-upload.ts
@@ -1,8 +1,8 @@
-import { failure } from '@libs/failure';
-import { DynamoDB } from '@aws-sdk/client-dynamodb';
-import { marshall } from '@aws-sdk/util-dynamodb';
 import { randomUUID } from 'node:crypto';
-import { FILES_TABLE_NAME } from '@libs/constants';
+import { failure } from '@infra/http/failure';
+import { getMetadataByKey } from '@infra/services/s3';
+import { save } from '@infra/repositories/files';
+import { IFileType } from '@contracts/file';
 
 const postProcessingFileUpload = async (key: string) => {
   const metadata = await getMetadataByKey(key);
@@ -13,18 +13,12 @@ const postProcessingFileUpload = async (key: string) => {
 
   const { title } = metadata;
 
-  const dynamodb = new DynamoDB({
-    endpoint: process.env.IS_OFFLINE ? 'http://localhost:4566' : undefined,
-  });
-  await dynamodb.putItem({
-    TableName: FILES_TABLE_NAME,
-    Item: marshall({
-      id: randomUUID(),
-      type: 'VIDEO',
-      title,
-      key,
-      createdAt: Date.now(),
-    }),
+  await save({
+    id: randomUUID(),
+    type: IFileType.VIDEO,
+    title,
+    key,
+    createdAt: Date.now(),
   });
 };
 
